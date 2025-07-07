bf4d8ecde6b30c0cf6ece3f09971ad3ba0441ac4
Su3rO <f.su3ro@gmail.com>
2025-07-07 19:13:54 +0200

    Fix compile errors with ical4j and JPA imports

--- src/main/java/com/example/icalendar/model/CalendarEvent.java
+++ src/main/java/com/example/icalendar/model/CalendarEvent.java
@@ -1,10 +1,10 @@
 package com.example.icalendar.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Column;
 import java.time.LocalDateTime;
 
 @Entity
