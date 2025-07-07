03d3bf57f4b810d49217688d10547fa06d46af59
Su3rO <f.su3ro@gmail.com>
2025-07-07 19:44:31 +0200

    Update Postgres port to 5436

--- src/main/resources/db/migration/V1__create_events_table.sql
+++ src/main/resources/db/migration/V1__create_events_table.sql
@@ -1,5 +1,5 @@
 CREATE TABLE calendar_event (
-    id SERIAL PRIMARY KEY,
+    id BIGSERIAL PRIMARY KEY,
     summary VARCHAR(255) NOT NULL,
     start TIMESTAMP NOT NULL,
     end_time TIMESTAMP NOT NULL
